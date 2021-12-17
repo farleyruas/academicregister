9665c57b78809b74f82b8d7094133bdd2caa7212
farley <farley.ruas@perficient.com>
2021-12-17 00:29:13 -0500

    fix(*): fixing database configuration

---
+++ src/main/java/com/academicregister/configuration/beans/ModelMapperBean.java
@@ -0,0 +1,34 @@
+package com.academicregister.configuration.beans;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+/**
+ * Model Mapper configuration.
+ */
+@Configuration
+public class ModelMapperBean {
+
+    /**
+     * ModelMapper bean declaration.
+     *
+     * @return ModelMapper Bean
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
